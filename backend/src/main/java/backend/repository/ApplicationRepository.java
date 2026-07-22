96e84edb3285ab79f0acc85010144652049af03f
ahamedhasankf-max <ahamedhasankf@gmail.com>
2026-07-22 11:38:44 +0530

    Some Changes Made in frontend

---
+++ backend/src/main/java/backend/repository/ApplicationRepository.java
@@ -0,0 +1,12 @@
+package backend.repository;
+
+import backend.entity.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+    Optional<Application> findByEmail(String email);
+
+}
